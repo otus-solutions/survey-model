d9f1116e245916847ed54eb28183951e2366ad36
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-09-30 21:03:26 -0300

    OTUS-21 Buscas Custom IDs

--- survey-model/src/main/java/org/ccem/otus/survey/template/SurveyTemplate.java
+++ survey-model/src/main/java/org/ccem/otus/survey/template/SurveyTemplate.java
@@ -1,9 +1,13 @@
 package org.ccem.otus.survey.template;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.ccem.otus.survey.template.identity.Identity;
 import org.ccem.otus.survey.template.item.SurveyItem;
+import org.ccem.otus.survey.template.item.questions.selectable.CheckboxQuestion;
 import org.ccem.otus.survey.template.metainfo.MetaInfo;
 import org.ccem.otus.survey.template.navigation.Navigation;
 import org.ccem.otus.survey.template.utils.adapters.SurveyItemAdapter;
@@ -13,29 +17,50 @@ import com.google.gson.GsonBuilder;
 
 public class SurveyTemplate {
 
-	public String extents;
-	public String objectType;
-	public String oid;
-	public Identity identity;
-	public MetaInfo metainfo;
-	public List<SurveyItem> itemContainer;
-	public List<Navigation> navigationList;
-	
-	public static SurveyTemplate deserialize(String surveyJson) {
-		GsonBuilder builder = new GsonBuilder();
-		builder.registerTypeAdapter(SurveyItem.class, new SurveyItemAdapter());
-		Gson gson = builder.create();
+    public String extents;
+    public String objectType;
+    public String oid;
+    public Identity identity;
+    public MetaInfo metainfo;
+    public List<SurveyItem> itemContainer;
+    public List<Navigation> navigationList;
 
-		return gson.fromJson(surveyJson, SurveyTemplate.class);
-	}
+    public List<String> getCustomIdItems() {
+        return itemContainer.stream()
+                .map(item -> item.customID)
+                .collect(Collectors.toList());
+    }
 
-	public static String serialize(SurveyTemplate survey) {
-		GsonBuilder builder = new GsonBuilder();
-		builder.registerTypeAdapter(SurveyItem.class, new SurveyItemAdapter());
-		builder.disableHtmlEscaping();
-		Gson gson = builder.create();
+    public List<String> getCustomIdOptions() {
+        return itemContainer.stream()
+                .filter(item -> item instanceof CheckboxQuestion)
+                .flatMap(item -> ((CheckboxQuestion) item).options.stream())
+                .map(option -> option.customOptionID)
+                .collect(Collectors.toList());
+    }
 
-		return gson.toJson(survey);
-	}
-	
+    public Set<String> getCustomIds(){
+        Set<String> customIds = new HashSet<>();
+        customIds.addAll(getCustomIdItems());
+        customIds.addAll(getCustomIdOptions());
+        return customIds;
+    }
+
+    public static SurveyTemplate deserialize(String surveyJson) {
+        Gson builder = SurveyTemplate.getGsonBuilder();
+        return builder.fromJson(surveyJson, SurveyTemplate.class);
+    }
+
+    public static String serialize(SurveyTemplate survey) {
+        Gson builder = SurveyTemplate.getGsonBuilder();
+        return builder.toJson(survey);
+    }
+
+    private static Gson getGsonBuilder(){
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(SurveyItem.class, new SurveyItemAdapter());
+        builder.disableHtmlEscaping();
+        return builder.create();
+    }
+
 }
--- survey-model/src/test/java/org/ccem/otus/survey/template/SurveyTemplateTest.java
+++ survey-model/src/test/java/org/ccem/otus/survey/template/SurveyTemplateTest.java
@@ -3,18 +3,24 @@ package org.ccem.otus.survey.template;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.ccem.otus.survey.template.SurveyTemplate;
 import org.ccem.otus.survey.template.identity.Identity;
+import org.ccem.otus.survey.template.item.SurveyItem;
+import org.ccem.otus.survey.template.item.questions.selectable.CheckboxQuestion;
+import org.ccem.otus.survey.template.item.questions.selectable.options.CheckboxOption;
 import org.ccem.otus.survey.template.metainfo.MetaInfo;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.google.gson.Gson;
+import org.powermock.api.mockito.PowerMockito;
 
 public class SurveyTemplateTest {
-	
 	private SurveyTemplate survey;
 	
 	@Before
@@ -42,5 +48,55 @@ public class SurveyTemplateTest {
 		assertTrue(survey.itemContainer instanceof List<?>);
 		assertTrue(survey.navigationList instanceof List<?>);
 	}
-	
+
+	@Test
+	public void should_return_customIds_all_items(){
+		List<SurveyItem> surveyItems = new ArrayList<>();
+		surveyItems.add(buildSurveyItem("ID1"));
+		surveyItems.add(buildSurveyItem("ID2"));
+		surveyItems.add(buildSurveyItem("ID3"));
+
+		SurveyTemplate surveyTemplate = new SurveyTemplate();
+		surveyTemplate.itemContainer = surveyItems;
+
+		List<String> foundedIds = surveyTemplate.getCustomIdItems();
+		Assert.assertFalse(foundedIds.isEmpty());
+		Assert.assertEquals(foundedIds.get(0), "ID1");
+		Assert.assertEquals(foundedIds.get(1), "ID2");
+		Assert.assertEquals(foundedIds.get(2), "ID3");
+	}
+
+	@Test
+	public void should_return_customIdsOption_all_QuestionCheckbox(){
+		List<SurveyItem> surveyItems = new ArrayList<>();
+		surveyItems.add(buildCheckboxQuestion("ID1a"));
+		surveyItems.add(buildCheckboxQuestion("ID2b"));
+		surveyItems.add(buildCheckboxQuestion("ID3c"));
+		surveyItems.add(buildCheckboxQuestion("ID4d"));
+
+		SurveyTemplate surveyTemplate = new SurveyTemplate();
+		surveyTemplate.itemContainer = surveyItems;
+
+		List<String> foundedIds = surveyTemplate.getCustomIdOptions();
+		Assert.assertFalse(foundedIds.isEmpty());
+		Assert.assertEquals(foundedIds.get(0), "ID1a");
+		Assert.assertEquals(foundedIds.get(1), "ID2b");
+		Assert.assertEquals(foundedIds.get(2), "ID3c");
+		Assert.assertEquals(foundedIds.get(3), "ID4d");
+	}
+
+	private CheckboxQuestion buildCheckboxQuestion(String customIdOption){
+		CheckboxQuestion checkboxQuestion = new CheckboxQuestion();
+		CheckboxOption checkboxOption = new CheckboxOption();
+		checkboxOption.customOptionID = customIdOption;
+		checkboxQuestion.options = Arrays.asList(checkboxOption);
+		return checkboxQuestion;
+	}
+
+	private SurveyItem buildSurveyItem(String customId){
+		SurveyItem surveyItem = new SurveyItem();
+		surveyItem.customID = customId;
+		return surveyItem;
+	}
+
 }
