eb6ce2ed5f8c712f04271f8fe647fc38e63d112b
brenoscheffer <brenoscheffer@gmail.com>
2017-10-27 19:39:20 -0200

    SM-5 Busca de metadado adicionada

--- survey-model/src/main/java/org/ccem/otus/survey/template/item/questions/metadata/MetadataGroup.java
+++ survey-model/src/main/java/org/ccem/otus/survey/template/item/questions/metadata/MetadataGroup.java
@@ -1,11 +1,16 @@
 package org.ccem.otus.survey.template.item.questions.metadata;
 
 import java.util.List;
+import java.util.Optional;
 
 public class MetadataGroup {
 
 	public String extents;
 	public String objectType;
 	public List<MetadataOption> options;
+
+	public Optional<MetadataOption> getMetadataByValue(Integer value){
+		return options.stream().filter(metadata -> metadata.value.equals(value)).findFirst();
+	}
 
 }
