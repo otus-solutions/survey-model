2856dd98bac7dcbf8a489ecccabda837375c0c88
emanoelvianna <emanoel.vianna@outlook.com>
2019-07-08 14:29:53 -0300

    SM-12 - adicionado editorconfig

--- survey-model/src/main/java/org/ccem/otus/survey/template/item/questions/fillingRules/ValidatorsMapping.java
+++ survey-model/src/main/java/org/ccem/otus/survey/template/item/questions/fillingRules/ValidatorsMapping.java
@@ -27,74 +27,75 @@ import org.ccem.otus.survey.template.item.questions.fillingRules.validators.time
 import org.ccem.otus.survey.template.item.questions.fillingRules.validators.time.MinTime;
 
 public enum ValidatorsMapping {
-	
-	Mandatory(Mandatory.class, "mandatory"),
-	Accept(Accept.class, "accept"),
 
-	/* CalendarQuestion */
-	PAST_DATE(PastDate.class, "pastDate"),
-	FUTURE_DATE(FutureDate.class, "futureDate"),
-	MIN_DATE(MinDate.class, "minDate"),
-	RANGE_DATE(RangeDate.class, "rangeDate"),
-	MAX_DATE(MaxDate.class, "maxDate"),
-	
-	/* IntegerQuestion */
-	DISTINCT(Distinct.class, "distinct"),
-	LOWER_LIMIT(LowerLimit.class, "lowerLimit"),
-	UPPER_LIMIT(UpperLimit.class, "upperLimit"),
-	PRECISION(Precision.class, "precision"),
-	IN(In.class, "in"),
-	
-	/* DecimalQuestion */
-	SCALE(Scale.class, "scale"),
-	
-	/* TextQuestion */
-	ALPHANUMERIC(Alphanumeric.class, "alphanumeric"),
-	LOWER_CASE(LowerCase.class, "lowerCase"),
-	UPPER_CASE(UpperCase.class, "upperCase"),
-	SPECIALS(Specials.class, "specials"),
-	MIN_LENGTH(MinLength.class, "minLength"),
-	MAX_LENGTH(MaxLength.class, "maxLength"),
-	
-	/* TimeQuestion */
-	MIN_TIME(MinTime.class, "minTime"),
-	MAX_TIME(MaxTime.class, "maxTime"),
-	
-    /* CheckboxQuestion */
-	QUANTITY(Quantity.class, "quantity"),
-    MIN_SELECTED(MinSelected.class, "minSelected"),
-    MAX_SELECTED(MaxSelected.class, "maxSelected");
-	
-	private Class<? extends GenericValidator> validator;
-	private String type;
-	
-	private ValidatorsMapping(Class<? extends GenericValidator> validator, String type) {
-		this.validator = validator;
-		this.type = type;
-	}
-	
-	public Class<? extends GenericValidator> getValidatorClass() {
-		return validator;
-	}
+  Mandatory(Mandatory.class, "mandatory"), 
+  Accept(Accept.class, "accept"),
 
-	public String getValidatorType() {
-		return type;
-	}
-	
-	public static ValidatorsMapping getEnumByObjectType(String objectType) {
-		ValidatorsMapping aux = null;
-		
-		for (ValidatorsMapping item : values()) {
-			if(item.getValidatorType().equals(objectType)) {
-				aux = item;
-			}
-		}
-		
-		if(aux == null) {
-			throw new RuntimeException("Error: " + objectType + " was not found at ValidatorsMapping.");
-		};
-		
-		return aux;
-	}
+  /* CalendarQuestion */
+  PAST_DATE(PastDate.class, "pastDate"), 
+  FUTURE_DATE(FutureDate.class, "futureDate"), 
+  MIN_DATE(MinDate.class, "minDate"), 
+  RANGE_DATE(RangeDate.class, "rangeDate"), 
+  MAX_DATE(MaxDate.class, "maxDate"),
+
+  /* IntegerQuestion */
+  DISTINCT(Distinct.class, "distinct"), 
+  LOWER_LIMIT(LowerLimit.class, "lowerLimit"), 
+  UPPER_LIMIT(UpperLimit.class, "upperLimit"), 
+  PRECISION(Precision.class, "precision"), 
+  IN(In.class, "in"),
+
+  /* DecimalQuestion */
+  SCALE(Scale.class, "scale"),
+
+  /* TextQuestion */
+  ALPHANUMERIC(Alphanumeric.class, "alphanumeric"), 
+  LOWER_CASE(LowerCase.class, "lowerCase"), 
+  UPPER_CASE(UpperCase.class, "upperCase"), 
+  SPECIALS(Specials.class, "specials"), 
+  MIN_LENGTH(MinLength.class, "minLength"), 
+  MAX_LENGTH(MaxLength.class, "maxLength"),
+
+  /* TimeQuestion */
+  MIN_TIME(MinTime.class, "minTime"), 
+  MAX_TIME(MaxTime.class, "maxTime"),
+
+  /* CheckboxQuestion */
+  QUANTITY(Quantity.class, "quantity"), 
+  MIN_SELECTED(MinSelected.class, "minSelected"), 
+  MAX_SELECTED(MaxSelected.class, "maxSelected");
+
+  private Class<? extends GenericValidator> validator;
+  private String type;
+
+  private ValidatorsMapping(Class<? extends GenericValidator> validator, String type) {
+    this.validator = validator;
+    this.type = type;
+  }
+
+  public Class<? extends GenericValidator> getValidatorClass() {
+    return validator;
+  }
+
+  public String getValidatorType() {
+    return type;
+  }
+
+  public static ValidatorsMapping getEnumByObjectType(String objectType) {
+    ValidatorsMapping aux = null;
+
+    for (ValidatorsMapping item : values()) {
+      if (item.getValidatorType().equals(objectType)) {
+        aux = item;
+      }
+    }
+
+    if (aux == null) {
+      throw new RuntimeException("Error: " + objectType + " was not found at ValidatorsMapping.");
+    }
+    ;
+
+    return aux;
+  }
 
 }
