b34b420c53aaa7426873fc61ff3131693bbc42ea
emanoelvianna <emanoel.vianna@outlook.com>
2019-07-26 15:26:02 -0300

    SM-13 - definição do modelo para as variaveis

---
+++ survey-model/src/main/java/org/ccem/otus/survey/staticVariable/CustomizationDefinition.java
@@ -0,0 +1,8 @@
+package org.ccem.otus.survey.staticVariable;
+
+public class CustomizationDefinition {
+
+  public String value;
+  public String label;
+
+}
---
+++ survey-model/src/main/java/org/ccem/otus/survey/staticVariable/StaticVariableDefinition.java
@@ -0,0 +1,15 @@
+package org.ccem.otus.survey.staticVariable;
+
+import java.util.List;
+
+public class StaticVariableDefinition {
+
+  public String objectType;
+  public String name;
+  public String label;
+  public Integer sending;
+  public Boolean bindToWholeTemplate;
+  public List<String> bindTo;
+  public List<CustomizationDefinition> customizations;
+
+}
